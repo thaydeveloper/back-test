33f25494ff7037a9cf67c5dca695ddef946fd58a
thaydeveloper <thaydeveloper26@gmail.com>
2025-02-04 21:07:15 -0300

    feature implemented controller criarCarro

--- src/controller/carros.ts
+++ src/controller/carros.ts
@@ -17,7 +17,13 @@ export const criarCarro = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  res.send("Criando um carro");
+  try {
+    const { marca, modelo, ano, cor, valor } = req.body;
+    await knex("carros").insert({ marca, modelo, ano, cor, valor });
+    res.status(201).json({ mensagem: "Carro criado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
 };
 
 export const atualizarCarro = async (
